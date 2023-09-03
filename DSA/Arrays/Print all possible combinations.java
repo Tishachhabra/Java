9a06c5916fb3355aca593486624f52f088ea4233
Tisha chhabra <tishachhabra2702@gmail.com>
2023-09-03 12:28:19 +0530

    Update Print all possible combinations.java

--- DSA/Arrays/Print all possible combinations.java
+++ DSA/Arrays/Print all possible combinations.java
@@ -11,19 +11,19 @@ r ---> Size of a combination to be printed
 //Use backtracking - Time complexity will be the order of depth of tree or O(n-r) 
 
 void combination(int arr[], int ans[], int start,int end, int index, int r)
+{
+	if (index == r)                                                       //base case ; combination ready
 	{
-		if (index == r)                                                       //base case ; combination ready
-		{
-			for (int j=0; j<r; j++)
-				{ System.out.print(ans[j]+" "); }
-			System.out.println("");
-			return;
-		}
-
-		for (int i=start; i<=end && end-i+1 >= r-index; i++)                 // replace ans[index] with all possible elements, condition : end-i+1 >= r-index
-                                                                          //make sure that no. of elements remaining to be traversed can make required combo
-		{
-			ans[index] = arr[i];
-			combination(arr, ans, i+1, end, index+1, r);
-		}
+		for (int j=0; j<r; j++)
+			{ System.out.print(ans[j]+" "); }
+		System.out.println("");
+		return;
 	}
+
+	for (int i=start; i<=end && end-i+1 >= r-index; i++)                 // replace ans[index] with all possible elements, condition : end-i+1 >= r-index
+								  										//make sure that no. of elements remaining to be traversed can make required combo
+	{
+		ans[index] = arr[i];
+		combination(arr, ans, i+1, end, index+1, r);
+	}
+}
