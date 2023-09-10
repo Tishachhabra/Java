e6a12750e533d21ae6c215dc152952d22ab10f78
Tisha chhabra <tishachhabra2702@gmail.com>
2023-09-11 00:07:18 +0530

    Create Longest Substring Without Repeating Characters.java

---
+++ DSA/Strings/Longest Substring Without Repeating Characters.java
@@ -0,0 +1,30 @@
+/*
+Link - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+*/
+
+// Set, 2 pointer approach
+public int lengthOfLongestSubstring(String s) 
+{
+    int n = s.length();
+    int maxLength = 0;
+    Set<Character> charSet = new HashSet<>();
+    int left = 0;
+    
+    for (int right = 0; right < n; right++) {
+        if (!charSet.contains(s.charAt(right))) 
+        {
+            charSet.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+         else 
+         {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+            charSet.add(s.charAt(right));
+        }
+    }
+    
+    return maxLength;
+}
