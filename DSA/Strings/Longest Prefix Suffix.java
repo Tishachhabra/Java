528d28aff59cc7bca4a66947c7ab26709c01f7ac
Tisha chhabra <tishachhabra2702@gmail.com>
2023-09-24 03:52:10 +0530

    Create Longest Prefix Suffix.java

---
+++ DSA/Strings/Longest Prefix Suffix.java
@@ -0,0 +1,50 @@
+/* 
+Link - https://practice.geeksforgeeks.org/problems/longest-prefix-suffix2527/1
+*/
+
+//Naive approach
+//all prefixes check with same length suffixes - O(n^3)
+int lps(String s) 
+{
+    int i=0,j=s.length()-1,count=0;
+    for(;i<s.length()-1;i++,j--)
+    {
+        if(s.substring(0,i+1).equals(s.substring(j,s.length())))          
+        {
+           count=Math.max(count,s.substring(0,i+1).length());                 //if prefix==suffix update count for largest
+        }
+    }
+    return count;
+}
+
+//Best approach
+//longest prefix suffix array - O(n)
+int lps(String s) 
+{
+    int n = s.length();
+    int[] lps = new int[n];
+    int i = 0, j =1;
+    lps[0] = 0;
+    while(j <n)
+    {
+      if(s.charAt(i) == s.charAt(j))
+      {
+          lps[j] = i + 1 ;
+          i++;
+          j++;
+      }
+      else
+      {
+        if(i == 0)
+        {
+          lps[j] = 0;
+          j++;
+        }
+        else
+        {
+          i = lps[i-1];
+        }
+      }
+    }
+    return lps[n-1];                        //not whole string can be the answer
+}
