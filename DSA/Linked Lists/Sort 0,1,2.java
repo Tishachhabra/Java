742c00645ad52a22062f9a1d0254bccc198675a9
Tisha chhabra <tishachhabra2702@gmail.com>
2023-10-26 23:47:47 +0530

    Create Sort 0,1,2.java

---
+++ DSA/Linked Lists/Sort 0,1,2.java
@@ -0,0 +1,35 @@
+public static Node sortList(Node head) 
+    {
+        int zero=0,one=0,two=0;
+        Node node=head;
+        while(node!=null)
+        {
+            if(node.data==0)
+            zero++;
+            else if(node.data==1)
+            one++;
+            else
+            two++;
+            node=node.next;
+        }
+        node=head;
+        while (zero > 0 ) 
+        {
+          node.data = 0;
+          node = node.next;
+          --zero;
+        }
+        while (one > 0 ) 
+        {
+          node.data = 1;
+          node = node.next;
+          --one;
+        }
+        while (two > 0 ) 
+        {
+            node.data = 2;
+            node = node.next;
+            --two;
+        }
+    return head;
+    }
