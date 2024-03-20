21263f49ff1272c851c5796e5a087bb8e56dec07
Tisha chhabra <tishachhabra2702@gmail.com>
2024-03-20 14:45:39 +0530

    Update removeNthFromEnd.java

--- DSA/Linked Lists/removeNthFromEnd.java
+++ DSA/Linked Lists/removeNthFromEnd.java
@@ -2,21 +2,22 @@
 Link - https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 */
 
+//slow and fast pointer will be kept at dist of n , when fast reaches null,slow must hve reached node to be deleted
 public ListNode removeNthFromEnd(ListNode head, int n) 
 {
     ListNode slow=head;
     ListNode fast=head;
   
-    for(int i=0;i<n;i++)                      //slow and fast remain at a distance of n
+    for(int i=1;i<=n;i++)                      //slow and fast remain at a distance of n
     fast=fast.next;
-    if(fast==null)
+    
+    if(fast==null)                            //corner case when head is to be removed
     return head.next;
     while(fast.next!=null)
     {
         slow=slow.next;
         fast=fast.next;
     }
-    ListNode ptr=slow.next;                 //remove
-    slow.next=ptr.next;
+    slow.next=slow.next.next;                        //remove slow.next node
     return head;
 }
