bda25106f6e0f5d46ceb6f88d4067276e6979c4a
Tisha chhabra <tishachhabra2702@gmail.com>
2024-08-06 04:40:42 +0530

    Update Longest subarray with sum divisible by K.java

--- DSA/Arrays/Longest subarray with sum divisible by K.java
+++ DSA/Arrays/Longest subarray with sum divisible by K.java
@@ -1,29 +1,30 @@
-/*
-Link - https://practice.geeksforgeeks.org/problems/longest-subarray-with-sum-divisible-by-k1259/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
-*/
-
 // Use auxiliay array of mods and hashmap - O(n)
-int longSubarrWthSumDivByK(int arr[], int n, int k)
-{
-    HashMap<Integer, Integer> map= new HashMap<Integer, Integer>();               //store frst occurrences of mods
-    int mod_arr[] = new int[n];                                                  //store (arr[0]....arr[i]) % k , isi se subarrays decide
-    int max_len = 0;
-    int curr_sum = 0;
-    
-    for (int i = 0; i < n; i++) 
+// Make aux array for storing remainders of (prefixsum % k) until i 
+//After getting current rem + check if 0 then find potential length of subarr + else check map if it doesnt contains then put with i as first occurence + else if map contains then calc length from that occurence till i.
+
+class Solution{
+    int longSubarrWthSumDivByK(int arr[], int n, int k)
     {
-        curr_sum += arr[i];
-        mod_arr[i] = ((curr_sum % k) + k) % k;                                //sum may be -ve so 2 modulo
+       
+        HashMap<Integer, Integer> map= new HashMap<Integer, Integer>();
+        int mod_arr[] = new int[n];                     // 'mod_arr[i]' stores (sum[0..i] % k)
+        int max_len = 0;
+        int curr_sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            curr_sum += arr[i];
+            mod_arr[i] = ((curr_sum % k) + k) % k;        // as the sum can be negative taking modulo twice
+            if (mod_arr[i] == 0)
+                max_len = i + 1;
 
-        if (mod_arr[i] == 0)
-            max_len = i + 1;
-
-        else if (map.containsKey(mod_arr[i]) == false)                       //if mod value hasnt come yet, will store only frst occurence to make longer subarr
-            map.put(mod_arr[i], i);
-
-        else
-            if (max_len < (i - map.get(mod_arr[i])))                        //compare for no of elements in subarray with max_len so far
-            max_len = i - map.get(mod_arr[i]);
+            else if (map.containsKey(mod_arr[i]) == false)    //if not occured until now, store with first occurence
+                map.put(mod_arr[i], i);
+            else
+                if (max_len < (i - map.get(mod_arr[i])))      //from that index till i remainder will be nullified
+                max_len = i - map.get(mod_arr[i]);
+        }
+        return max_len;
     }
-    return max_len;
+ 
 }
+
