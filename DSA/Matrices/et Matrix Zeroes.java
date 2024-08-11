ec89a685912f0dfcc6e4d4d972f666d2f852e505
Tisha chhabra <tishachhabra2702@gmail.com>
2024-08-12 04:28:30 +0530

    Create et Matrix Zeroes.java

---
+++ DSA/Matrices/et Matrix Zeroes.java
@@ -0,0 +1,28 @@
+// Find 0s fill row[] and col[] + matrix traverse wherever row[i]==1 || col[i]==1 then fill matrix[i][j]==0
+
+class Solution {
+    public void setZeroes(int[][] matrix) 
+    {
+        int row[]= new int[matrix.length];
+        int col[]= new int[matrix[0].length];
+        
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<row.length;i++){
+            for(int j=0;j<col.length;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
