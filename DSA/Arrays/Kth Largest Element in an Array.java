9c5c56c90c0f0150031f7b6447da219741f6a902
Tisha chhabra <tishachhabra2702@gmail.com>
2024-08-19 17:11:00 +0530

    Create Kth Largest Element in an Array.java

---
+++ DSA/Arrays/Kth Largest Element in an Array.java
@@ -0,0 +1,18 @@
+// Use priority queue as max heap +with custom comparator + add all elements + remove (k-1) elements then by peek() =answer
+
+    public static int findKthLargest(int[] nums, int k) 
+  {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());          // also (a,b)=>b-a
+        // Add all elements of the array into the max-heap
+        for (int num : nums) {
+            maxHeap.add(num);
+        }
+        
+        // Remove the first (k-1) largest elements from the heap
+        for (int i = 0; i < k - 1; i++) {
+            maxHeap.poll();
+        }
+        
+        // The root of the heap now contains the kth largest element
+        return maxHeap.peek();
+  }
