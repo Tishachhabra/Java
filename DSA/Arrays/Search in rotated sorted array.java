040d12d5e2255ac465e945a4550cc9028f68f2ee
Tisha chhabra <tishachhabra2702@gmail.com>
2024-08-26 13:53:35 +0530

    Update Search in rotated sorted array.java

--- DSA/Arrays/Search in rotated sorted array.java
+++ DSA/Arrays/Search in rotated sorted array.java
@@ -14,7 +14,7 @@ public class Solution
                 return mid;
 
           //either mid is in real array or in rotated one
-            if (nums[low] <= nums[mid])                                //real array
+            if (nums[low] <= nums[mid])                                //rotated array
             {
                 if (nums[low] <= target && target <= nums[mid])        //target is in left section section , checking target boundaries
                     high = mid - 1;
@@ -22,7 +22,7 @@ public class Solution
                     low = mid + 1;
             } else 
             {
-                if (nums[mid] <= target && target <= nums[high])      //in roated 
+                if (nums[mid] <= target && target <= nums[high])      //in real 
                     low = mid + 1;
                 else
                     high = mid - 1;
