6248fc75982a66c468983019f93adfa3f45fb284
Tisha chhabra <tishachhabra2702@gmail.com>
2024-09-02 18:44:57 +0530

    Create Number Types.java

---
+++ DSA/Utilities/Number Types.java
@@ -0,0 +1,19 @@
+// Binary to Decimal
+String binaryString = "1010";                                       // Binary representation of 10
+int decimalValue = Integer.parseInt(binaryString, 2);
+
+
+// Decimal to Binary
+int decimalValue = 10;
+String binaryString = Integer.toBinaryString(decimalValue);
+
+
+// Octal to Decimal
+String octalString = "12";                                         // Octal representation of 10 in decimal
+int decimalValue = Integer.parseInt(octalString, 8);
+
+//Decimal to octal
+int decimalValue = 10;
+String octalString = Integer.toOctalString(decimalValue);
+
+
