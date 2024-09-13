85bc91f30341e14dfdd459145119bbe40330463f
Tisha chhabra <tishachhabra2702@gmail.com>
2024-09-13 15:22:09 +0530

    Update Longest Substring Without Repeating Characters.java

--- DSA/Strings/Longest Substring Without Repeating Characters.java
+++ DSA/Strings/Longest Substring Without Repeating Characters.java
@@ -28,3 +28,32 @@ public int lengthOfLongestSubstring(String s)
     
     return maxLength;
 }
+
+
+// Store char with index in map then update start whenever found duplicate ( less map opertions)
+
+
+public int lengthOfLongestSubstring(String s) {
+if (s.isEmpty()) return 0;
+        
+        int max = 0, start = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            // If character already seen, update start position
+            if (map.containsKey(ch) && map.get(ch) >= start)                     //if duplicate is within window
+            {
+                start = map.get(ch) + 1;
+            }
+            
+            // Update the character index
+            map.put(ch, i);
+            
+            // Update max length
+            max = Math.max(max, i - start + 1);
+        }
+        
+        return max;
+}
