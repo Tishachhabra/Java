32825db6a177f48b0e2b81d01f71a92fbd73fd41
Tisha chhabra <tishachhabra2702@gmail.com>
2025-09-18 18:31:20 +0530

    Update Next Greater Element.java

--- DSA/Arrays/Next Greater Element.java
+++ DSA/Arrays/Next Greater Element.java
@@ -4,17 +4,17 @@ public ArrayList<Integer> nextLargerElement(int[] arr) {
         Stack<Integer> st = new Stack<>();
         
         for (int i = arr.length - 1; i >= 0; i--) {
-            while (!st.isEmpty() && st.peek() <= arr[i]) //top bigger than curr ; if not delete smallers till find bigger
+            while (!st.isEmpty() && st.peek() <= arr[i])                       // pop out smaller numbers which appeared in right till found bigger or stack is empty
             {
                 st.pop();
             }
-            if (st.isEmpty()) // no one bigger than itself
+            if (st.isEmpty())                                            // stack got empty by popping / i is at last index == no element in right exist which is bigger than cur
             {
                 res.add(0, -1);
             } 
             else 
             {
-                res.add(0, st.peek());  //next bigger add
+                res.add(0, st.peek());                                  //next bigger left in stack add
             }
             st.push(arr[i]);
         }
